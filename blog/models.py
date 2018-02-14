079e5a1c0c1ec7a836188620e5375527073885f9
rexfordnana <rexfordnana@gmail.com>
2018-02-13 20:16:36 -0500

    Slight update to models.py

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    content = models.TextField(default="")
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
     image = models.ImageField(blank=True, null=True)
